e79055b6a8335af95b7a5d7b41aa447c80ac6b44
Mayara Erculano <mayara.correa59@gmail.com>
2025-10-24 18:30:54 -0300

    Versão inicial da plataforma de ONGs

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    alert("Obrigado por se interessar em ser voluntário!");
+  });
+});
